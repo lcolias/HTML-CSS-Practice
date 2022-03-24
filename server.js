ab839ea5ac6c5855a3519e6fe0722bde77aeb31c
Luke C <lpcolias@gmail.com>
2022-03-23 18:16:14 -0700

    New public

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     const count = readFileSync('./visitorCount.txt');
